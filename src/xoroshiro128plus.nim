1389b4a8b65658619fdee6db713c0bc06d28247d
ASC2011 <andreas.schoeller@icloud.com>
2024-03-12 10:18:29 +0100

    reduced doc-comment

--- src/xoroshiro128plus.nim
+++ src/xoroshiro128plus.nim
@@ -1,19 +1,8 @@
 
 ##[
 This implementation of `xoroshiro128+ <https://en.wikipedia.org/wiki/Xoroshiro128%2B>`_ was done by
-Mark Blacher et al. It was published 2021 in their paper *'Fast and Robust Vectorized In-Place Sorting of Primitive Types'* [Blacher2021]_.
-It provides pseudo-randomness for their vectorized Quicksort Algorithm by the name **VQSort**.
-It is part of the `"Fast and Robust"-repository <https://github.com/simd-sorting/fast-and-robust>`_.
-Following this `blog-post <https://opensource.googleblog.com/2022/06/Vectorized%20and%20performance%20portable%20Quicksort.html>`_
-the further development of the *VQSort*-algorithm goes on at Googles Brain group under the supervision
-of Jan Wassenberg. The newer codebase uses Googles' Highway-library and remains open
-as a contribution to sorting at `github/google/highway/contrib/sort <https://github.com/google/highway/tree/master/hwy/contrib/sort>`_.
-Within this preprint [Blacher20220513]_ the authors describe their general progress on *VQSort* and the changes
-to pseudo random-generation in detail. Note this code was adapted from the earlier publication *'Fast and Robust'* in 2021.
-
+Mark Blacher et al.
 ..   [Blacher2021] Blacher, Giesen, und Kühne, „Fast and Robust Vectorized In-Place Sorting of Primitive Types“ doi 10.4230/LIPIcs.SEA.2021.3
-..   [Blacher20220513] „Vectorized and performance-portable Quicksort“. retrieved 10.march.2024
-
  ]##
 
 import nimsimd/avx2
